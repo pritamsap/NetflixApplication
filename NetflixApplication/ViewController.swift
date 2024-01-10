155196b371ac61e3deb009094ca6f8a54641ee42
pritamsap <pritamsap2@gmail.com>
2024-01-10 18:23:33 -0500

    Initial Commit

---
+++ NetflixApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixApplication
+//
+//  Created by pritam on 2024-01-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

57a97772443589faebf8d2cbfbbf8f527b88db81
pritamsap <pritamsap2@gmail.com>
2024-01-11 15:21:40 -0500

    Implemented git ignore

---
+++ NetflixApplication/Controllers/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  NetflixApplication
+//
+//  Created by pritam on 2024-01-10.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
